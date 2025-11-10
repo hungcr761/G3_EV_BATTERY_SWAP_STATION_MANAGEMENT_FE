9da22a381c1b75517dd938158357f369263d52e6
Lê Hồng Minh <leehongminh004@gmail.com>
2025-11-10 23:58:54 +0700

    Update VIN validation to allow alphanumeric VIS

--- src/lib/apiServices.js
+++ src/lib/apiServices.js
@@ -175,6 +175,7 @@ export const transferAPI = {
 
 // Cabinet API (Station cabinets with slots & batteries)
 export const cabinetAPI = {
-    getAll: (params = {}) => api.get('/cabinets', { params })
+    getAll: (id) => api.get(`/cabinets${id}`)
+
 };
 
